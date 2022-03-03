5fe617296a586c1f73b236ad3ea7b28ab5f9e7e6
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-03 00:03:19 -0600

    Update bunny producer to generate a random transaction message

--- bunny_producer.rb
+++ bunny_producer.rb
@@ -9,9 +9,10 @@ channel = conn.create_channel
 queue = channel.queue("tx_dashboard", :auto_delete => false, :durable => true)
 exchange = channel.default_exchange
 
-transaction = "{\"account\": \"251678\", \"type\": {\"withdraw\": \"deposit\"}, \"origin\": {\"counter\": \"transfer\"}, \"concept\": \"Some snacks\", \"amount\": 3671.21, \"currency\": \"MXN\"}"
-
 [1, 2, 3, 4, 5].each do |i|
+  random_amount = rand(1000.00..3000.00)
+  transaction = "{\"account\": \"123\", \"type\": \"deposit\", \"origin\": \"transfer\", \"concept\": \"Some snacks\", \"amount\": #{random_amount}, \"currency\": \"MXN\"}"
+
   exchange.publish(transaction, :routing_key => queue.name)
 end
 
