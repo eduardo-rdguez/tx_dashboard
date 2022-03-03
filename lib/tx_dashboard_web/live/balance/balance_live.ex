2868d1f102c701fa89328687a0706a11dfe1fd01
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-03 00:07:18 -0600

    Add phoenix pubsub to subscribe tx in balance live view

--- lib/tx_dashboard_web/live/balance/balance_live.ex
+++ lib/tx_dashboard_web/live/balance/balance_live.ex
@@ -3,9 +3,19 @@ defmodule TxDashboardWeb.Balance.BalanceLive do
 
   alias TxDashboard.Dashboard.Transactions
 
+  @topic "transactions"
+
   @impl true
   def mount(%{"account_number" => account_number} = _params, _session, socket) do
-    transactions = Transactions.list_transactions()
+    Phoenix.PubSub.subscribe(TxDashboard.PubSub, @topic <> ":" <> account_number)
+    transactions = Transactions.find_all_by_account_number(account_number)
+
     {:ok, assign(socket, account_number: account_number, transactions: transactions)}
+  end
+
+  @impl true
+  def handle_info(message, socket) do
+    IO.inspect(message)
+    {:noreply, socket}
   end
 end
