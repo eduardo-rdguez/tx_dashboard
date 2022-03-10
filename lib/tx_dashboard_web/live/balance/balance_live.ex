1dcb35bfd04b1801f5abdc09779813b6e7488a7d
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-09 21:06:43 -0600

    Update balance live to list transactions by page params

--- lib/tx_dashboard_web/live/balance/balance_live.ex
+++ lib/tx_dashboard_web/live/balance/balance_live.ex
@@ -5,13 +5,35 @@ defmodule TxDashboardWeb.Balance.BalanceLive do
   alias TxDashboard.Schema.Transaction
 
   @topic "transactions"
+  @size_pages [5, 10, 15]
 
   @impl true
   def mount(%{"account_number" => account_number} = _params, _session, socket) do
     Phoenix.PubSub.subscribe(TxDashboard.PubSub, @topic <> ":" <> account_number)
-    transactions = Transactions.find_all_by_account_number(account_number)
 
-    {:ok, assign(socket, account_number: account_number, transactions: transactions)}
+    socket =
+      socket
+      |> assign(
+        account_number: account_number,
+        transactions: [],
+        size_pages: @size_pages
+      )
+
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(params, _uri, %{assigns: %{account_number: account_number}} = socket) do
+    page_params = page_params(params)
+    page = Transactions.list_by_account_number(account_number, page_params)
+    page_params = page_params |> Map.put(:total_pages, page.total_pages)
+
+    socket =
+      socket
+      |> assign(:transactions, page.entries)
+      |> assign(:page_params, page_params)
+
+    {:noreply, socket}
   end
 
   @impl true
@@ -21,5 +43,12 @@ defmodule TxDashboardWeb.Balance.BalanceLive do
       |> update(:transactions, &[transaction | &1])
 
     {:noreply, socket}
+  end
+
+  defp page_params(params) do
+    %{
+      page: params["page"] || 1,
+      page_size: params["page_size"] || 5
+    }
   end
 end
