1feaf3734377bcc01b1469c7a9d4a40d3614c5c4
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-11 11:58:59 -0600

    Add balance table component

---
+++ lib/tx_dashboard_web/live/balance/components/balance_table_component.ex
@@ -0,0 +1,47 @@
+defmodule TxDashboardWeb.Balance.Components.BalanceTableComponent do
+  use TxDashboardWeb, :live_component
+
+  @size_pages [5, 10, 15]
+
+  @impl true
+  def mount(socket) do
+    socket =
+      socket
+      |> assign(:size_pages, @size_pages)
+
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("paginate", %{"page" => page}, socket) do
+    socket =
+      socket.assigns.page_params
+      |> Map.replace!(:page, String.to_integer(page))
+      |> push_params(socket)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("select-page-size", %{"page-size" => page_size}, socket) do
+    socket =
+      socket.assigns.page_params
+      |> Map.replace!(:page_size, String.to_integer(page_size))
+      |> push_params(socket)
+
+    {:noreply, socket}
+  end
+
+  defp push_params(page_params, %{assigns: %{account_number: account_number}} = socket) do
+    push_patch(socket,
+      to:
+        Routes.balance_path(
+          socket,
+          :index,
+          account_number,
+          page: page_params.page,
+          page_size: page_params.page_size
+        )
+    )
+  end
+end
